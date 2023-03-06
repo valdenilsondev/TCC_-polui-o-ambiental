14eae95bccf7fc4b4b2bbcd69141811f2f0ebb64
Roberta <roberta.ferreira.santos79@gmail.com>
2023-03-06 08:56:44 -0300

    crie movimentação andar

--- TCC/Assets/script/MovimentoHorizontal.cs
+++ TCC/Assets/script/MovimentoHorizontal.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MovimentoHorizontal : MonoBehaviour
 {
+
+    public float velocidade;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class MovimentoHorizontal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float movimentoHorizntal = Input.GetAxisRaw("Horizontal");
+        float movimentoVertical = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector3(1 * movimentoHorizntal, 1 * movimentoVertical, 0) * velocidade);
     }
 }
