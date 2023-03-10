603ae7b028ff7ddd6efcbe1ba9f43cddf2d46948
Roberta <roberta.ferreira.santos79@gmail.com>
2023-03-10 14:35:02 -0300

    cria script abaixar

--- TCC/Assets/script/MovimentoHorizontal.cs
+++ TCC/Assets/script/MovimentoHorizontal.cs
@@ -5,33 +5,41 @@ using UnityEngine;
 
 public class MovimentoHorizontal : MonoBehaviour
 {
-    
+
     public float velocidade;
     public float JumpForce;
     public bool isJumping;
     private Rigidbody2D rig;
-   
+    private bool abaixar;
+    public Animator anim;
+   public float movimentoHorizntal;
+   public float movimentoVertical;
 
+    public SpriteRenderer spRender;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        
+        anim = GetComponentInChildren<Animator>();
+        spRender = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
 
         Jump();
         movimento();
-       
+        slide();
+
     }
 
 
     //Minhas funções
+
+
     #region 
 
     void Jump()
@@ -44,24 +52,25 @@ public class MovimentoHorizontal : MonoBehaviour
 
     void slide()
     {
-  if(Input.GetButton("slide")){
-            {
+        if (Input.GetKeyDown(KeyCode.DownArrow) )
+        {
+            
 
-            }
+            anim.SetTrigger("slide");
+
+        }
+        else
+        {
+            spRender.flipX = false;
+        }
     }
-      
-
-
-
-
-
 
     void movimento()
     {
-        float movimentoHorizntal = Input.GetAxisRaw("Horizontal");
-        float movimentoVertical = Input.GetAxisRaw("Vertical");
+        movimentoHorizntal = Input.GetAxisRaw("Horizontal");
+        movimentoVertical = Input.GetAxisRaw("Vertical");
 
-       
+
 
         transform.Translate(new Vector3(1 * movimentoHorizntal, 1 * movimentoVertical, 0) * velocidade * Time.deltaTime);
 
